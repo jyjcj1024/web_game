2da5d59029ac6c20ad4096d81a1eb977e9251ca5
jyjcj1024 <jyjcj1024@gmail.com>
2025-10-10 16:08:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function loadGame(url) {
-  // 현재 설정된 언어 정보를 가져옵니다.
-  const currentLang = i18next.language;
-  // URL에 쿼리 파라미터로 언어 정보를 추가합니다. (예: .../puzzles.html?lang=ko)
+  const currentLang = i18next.language || 'ko';
   document.getElementById('gameFrame').src = `${url}?lang=${currentLang}`;
 }
