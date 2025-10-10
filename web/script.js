26cdbf35cc215b496ca1af163d294e70e8507790
jyjcj1024 <jyjcj1024@gmail.com>
2025-10-10 15:31:37 +0900

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -1,3 +1,6 @@
 function loadGame(url) {
-  document.getElementById('gameFrame').src = url;
+  // 현재 설정된 언어 정보를 가져옵니다.
+  const currentLang = i18next.language;
+  // URL에 쿼리 파라미터로 언어 정보를 추가합니다. (예: .../puzzles.html?lang=ko)
+  document.getElementById('gameFrame').src = `${url}?lang=${currentLang}`;
 }
